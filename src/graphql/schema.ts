e2a663fb00d2ff16d6cc7f26ded53b12517f2354
João Neto <joao.neto.ninja@hotmail.com>
2019-02-03 11:54:34 -0300

    removendo resolvers triviais

--- src/graphql/schema.ts
+++ src/graphql/schema.ts
@@ -24,11 +24,6 @@ const typeDefs = `
     }
 `
 const resolvers = {
-    User: {
-        id: (user) => user.id,
-        name: (user) => user.name,
-        email: (user) => user.email
-    },
     Query: {
         allUsers: () => users
     },
